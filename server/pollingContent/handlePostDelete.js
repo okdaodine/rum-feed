b89dd1558972e962481b82b3c41d74eba6dee34b
okdaodine <btc.link.grt@gmail.com>
2023-03-14 15:11:12 +0800

    tweaks

--- server/pollingContent/handlePostDelete.js
+++ server/pollingContent/handlePostDelete.js
@@ -1,5 +1,6 @@
 const Post = require('../database/post');
 const rumSDK = require('rum-sdk-nodejs');
+const Orphan = require('../database/sequelize/orphan');
 
 module.exports = async (item) => {
   const {
@@ -14,7 +15,12 @@ module.exports = async (item) => {
   const userAddress = rumSDK.utils.pubkeyToAddress(SenderPubkey);
   const deletedPost = await Post.get(id);
   if (!deletedPost) {
-    return;
+    await Orphan.create({
+      content: item,
+      groupId: item.GroupId,
+      parentId: `${id}`,
+    });
+    throw new Error('Orphan');
   }
   if (userAddress !== deletedPost.userAddress) {
     return;
